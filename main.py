b63e848194e71252cbfc5b3910f117fad1bab502
birukabel <biruk.abel@yahoo.com>
2023-01-20 11:49:36 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from  replace_words import replace_words as rw
+
+worre = rw()
+worre.replace_word()
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
